43d0b4114950bd95a7648e42bc77dae25c94e445
smilillo <sofiemilillo@gmail.com>
2023-06-15 21:50:22 -0400

    added more file structure and updated server.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import Apollo
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
