a161b3fe64f7bf534c40d11ea9f630c072e61f21
smilillo <sofiemilillo@gmail.com>
2023-06-26 22:15:04 -0400

    update signupform

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -33,7 +33,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser(userFormData);
 
       if (!response.ok) {
         throw new Error('something went wrong!');
