80880fa534a7be5677205d35ad41420b1250e78a
smilillo <sofiemilillo@gmail.com>
2023-07-10 21:16:25 -0400

    working on signup issues

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,14 @@
-const { Book, User } = require('../models')
+const { Book, User } = require('../models');
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
     Query: {
         me: async (parent, args, context) => {
             if (context.user) {
-                const userData = await User.findOne({ _id: context.user._id })
+                const user = await User.findOne({ _id: context.user._id })
                     .select('-__v -password')
-            return userData;
+            return user;
             }
 
             throw new AuthenticationError('Not logged in');
@@ -28,10 +30,11 @@ const resolvers = {
             }
       
             const token = signToken(user);
+
             return { token, user };
         },
-        addUser: async (parent, args) => {
-            const user = await User.create(args);
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
         },
